d2dabcc94356e45d7bf664361d01d65e2b9c830a
Srihari1798 <120098197+Srihari1798@users.noreply.github.com>
2023-04-08 21:51:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,54 @@
-//your JS code here. If required.
+// //your JS code here. If required.
+// function manipulateData(array) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve(array);
+//     }, 3000);
+//   });
+// }
+
+// manipulateData([1,3,5,8,10])
+//   .then((array) => {
+//     return new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//         const filteredArray = array.filter((num) => num % 2 === 0);
+//         resolve(filteredArray);
+//       }, 1000);
+//     });
+//   })
+//   .then((filteredArray) => {
+//     return new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//         const mappedArray = filteredArray.map((num) => num * 2);
+//         resolve(mappedArray);
+//       }, 2000);
+//     });
+//   })
+//   .then((mappedArray) => {
+//     const outputElement = document.getElementById("output");
+//     outputElement.textContent = mappedArray.join(", ");
+//   });
+
+
+const inputArray = [1, 2, 3, 4];
+
 function manipulateData(array) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve(array);
+      const filteredArray = array.filter((num) => num % 2 === 0);
+      const mappedArray = filteredArray.map((num) => (num % 2 === 0 ? num * 2 : num));
+      resolve(mappedArray);
     }, 3000);
   });
 }
 
-manipulateData([1,3,5,8,10])
-  .then((array) => {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        const filteredArray = array.filter((num) => num % 2 === 0);
-        resolve(filteredArray);
-      }, 1000);
-    });
-  })
-  .then((filteredArray) => {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        const mappedArray = filteredArray.map((num) => num * 2);
-        resolve(mappedArray);
-      }, 2000);
-    });
-  })
-  .then((mappedArray) => {
-    const outputElement = document.getElementById("output");
-    outputElement.textContent = mappedArray.join(", ");
-  });
+const outputElement = document.getElementById("output");
+
+manipulateData(inputArray).then((resultArray) => {
+  setTimeout(() => {
+    outputElement.textContent = resultArray.join(", ");
+  }, 1000);
+  setTimeout(() => {
+    outputElement.textContent = resultArray.join(", ");
+  }, 2000);
+});
