6f8cad3c0ff5a5b4732114ad19997f241e85a4a8
Srihari1798 <120098197+Srihari1798@users.noreply.github.com>
2023-04-08 21:45:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+function manipulateData(array) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(array);
+    }, 3000);
+  });
+}
+
+manipulateData([1, 2, 3, 4])
+  .then((array) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const filteredArray = array.filter((num) => num % 2 === 0);
+        resolve(filteredArray);
+      }, 1000);
+    });
+  })
+  .then((filteredArray) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const mappedArray = filteredArray.map((num) => num * 2);
+        resolve(mappedArray);
+      }, 2000);
+    });
+  })
+  .then((mappedArray) => {
+    const outputElement = document.getElementById("output");
+    outputElement.textContent = mappedArray.join(", ");
+  });
